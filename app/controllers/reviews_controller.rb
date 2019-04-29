c24da36880b7ff974c509ef37a1cc2452a114dcd
riririusei99 <riry1313@gmail.com>
2019-04-29 06:34:43 +0000

    fix bug not redirecting review#new action.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < RankingController
-  before_action :authenticate_user!, only: new
+  before_action :authenticate_user!, only: :new
   
   def new
     @product = Product.find(params[:product_id])
