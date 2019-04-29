d6251c16c2a0165cedf563219ad65a603cfa7027
riririusei99 <riry1313@gmail.com>
2019-04-29 08:48:42 +0000

    remove nickname from review & migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_29_065645) do
+ActiveRecord::Schema.define(version: 2019_04_29_084749) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2019_04_29_065645) do
   end
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname"
     t.integer "rate"
     t.text "review"
     t.integer "product_id"
